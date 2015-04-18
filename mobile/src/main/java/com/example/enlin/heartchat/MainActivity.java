181d0b8a4d4e2461b4d246e10e5c543c67336ee6
jannle <jannleno1@gmail.com@10.10.192.254>
2015-04-18 10:43:35 -0400

    Enlin
    Remove the test comments

--- mobile/src/main/java/com/example/enlin/heartchat/MainActivity.java
+++ mobile/src/main/java/com/example/enlin/heartchat/MainActivity.java
@@ -43,8 +43,6 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-            //testCommit
-            //testCommit2
         }
 
         return super.onOptionsItemSelected(item);
